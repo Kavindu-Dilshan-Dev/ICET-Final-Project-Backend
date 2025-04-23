0389092f4e48b7500142eeaf79731a8219cc5a15
Kavindu Dilshan <kavindu4543@gmail.com>
2025-04-23 21:11:21 +0530

    Project Initialization and Main Configurations Completed

---
+++ src/main/java/com/kavindu/security/KeycloackJwtAuthenticationConverter.java
@@ -0,0 +1,34 @@
+package com.kavindu.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class KeycloackJwtAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    @Override
+    public AbstractAuthenticationToken convert(@NonNull Jwt source) {
+        return new JwtAuthenticationToken(source,
+                Stream.concat(new JwtGrantedAuthoritiesConverter().convert(source).stream(),
+                        extractResourceRoles(source).stream()
+                        ).collect(Collectors.toSet())
+
+                );
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        var recourceAccess = new HashMap<>(jwt.getClaim("resource_access"));
+
+        var eternal = recourceAccess.get("account");
+
+    }
+}
