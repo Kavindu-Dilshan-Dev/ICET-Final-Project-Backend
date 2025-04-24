0854c93991f6e46ee2be709efa9dd7c119bd8208
Kavindu Dilshan <kavindu4543@gmail.com>
2025-04-24 07:14:10 +0530

    Chat Service Implimentation Completed

---
+++ src/main/java/com/kavindu/common/StringResponse.java
@@ -0,0 +1,12 @@
+package com.kavindu.common;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StringResponse {
+    private String response;
+}
